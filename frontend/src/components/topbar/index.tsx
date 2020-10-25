1a0fa3e7714aba65f00503f8a3e23bebc2aa89b6
Carlos Oliveira <carlos.oliveira@bitmaker-software.com>
2020-10-25 16:16:18 +0000

    FontAwesomeIcons

--- frontend/src/components/topbar/index.tsx
+++ frontend/src/components/topbar/index.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoins, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 import { StoreState, User } from "../../store";
 
@@ -24,9 +26,24 @@ class TopbarComponent extends React.Component<TopbarProps, {}> {
           <Nav className="mr-auto">
             <Link to="/products">Catalog</Link>
           </Nav>
-          <Navbar.Text>
-            {user && `${user.username} - ${user.balance}`}
-          </Navbar.Text>
+          {user && (
+            <>
+              <Navbar.Text style={{ marginRight: "8px" }}>
+                {`${user.username} - ${user.balance}`}
+              </Navbar.Text>
+              <FontAwesomeIcon icon={faCoins} style={{ marginRight: "8px" }} />
+              <Link to="/checkout">
+                <div style={{ position: "relative" }}>
+                  <span
+                    style={{ position: "absolute", bottom: "-5px", left: "-3px", fontSize: '12px' }}
+                  >
+                    0
+                  </span>
+                  <FontAwesomeIcon icon={faShoppingCart} />
+                </div>
+              </Link>
+            </>
+          )}
         </Navbar.Collapse>
       </Navbar>
     );
