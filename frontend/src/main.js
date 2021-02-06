c906dee49f9fecd6d837693e85b328f2a5843abe
André Ventura <andre.ventura@bitmaker-software.com>
2021-02-06 17:10:42 +0000

    Project dependencies: add bootstrap and bootstrap-vue.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import BootstrapVue from 'bootstrap-vue';
 
 import App from './App.vue';
 import router from './router';
@@ -9,6 +10,7 @@ import store from './store';
 Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
