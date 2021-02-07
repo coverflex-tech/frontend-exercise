6fff50ec0472db9ee2a629bfd3eabcd2a3ec2d49
André Ventura <andre.ventura@bitmaker-software.com>
2021-02-07 19:53:36 +0000

    Home view: add user info and product list.

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--    <img alt="Vue logo" src="../assets/logo.png">-->
+    <template v-if="!isAuthenticated">
+      <b-jumbotron
+        header="Benefits"
+        lead="Hi there,"
+      >
+        <p>
+          sign in to see and manage your benefits!
+        </p>
+        <b-button
+          v-if="!signInPopoverIsOpen"
+          variant="primary"
+          @click="updateSignInPopoverOpenStatus(true)"
+        >
+          login
+        </b-button>
+      </b-jumbotron>
+    </template>
+    <template v-else>
+      <user-info
+        class="m-4"
+      />
+      <product-list
+        class="m-4"
+      />
+    </template>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
+
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import ProductList from '@/components/ProductList.vue';
+import UserInfo from '@/components/UserInfo.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    ProductList,
+    UserInfo,
+  },
+  computed: {
+    ...mapGetters({
+      isAuthenticated: 'auth/isAuthenticated',
+      signInPopoverIsOpen: 'auth/signInPopoverIsOpen',
+      userId: 'auth/userId',
+    }),
+  },
+  methods: {
+    ...mapActions({
+      updateSignInPopoverOpenStatus: 'auth/updateSignInPopoverOpenStatus',
+    }),
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.jumbotron {
+  //background-color: #F5EBE3;
+  background-color: transparent;
+}
+</style>
