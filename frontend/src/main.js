9d4d7384b838bb9a9f5f75c18fe86580bd4c1762
André Ventura <andre.ventura@bitmaker-software.com>
2021-02-08 13:38:48 +0000

    Setup Vue Moment and Font Awesome Icons.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 import BootstrapVue from 'bootstrap-vue';
 import VueLogger from 'vuejs-logger';
+import VueMoment from 'vue-moment';
+
+import { library as faLibrary } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from './App.vue';
 import router from './router';
@@ -12,6 +16,8 @@ Vue.config.productionTip = false;
 
 Vue.use(VueAxios, axios);
 Vue.use(BootstrapVue);
+
+Vue.use(VueMoment);
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -28,6 +34,29 @@ Vue.use(VueLogger, {
 // Initialize stores
 store.dispatch('auth/initialize');
 store.dispatch('products/initialize');
+
+//
+// Font Awesome Icons
+//
+/* eslint-disable import/first, import/order */
+import {
+  faCheck,
+  faExclamationTriangle,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons';
+
+// import {
+//   faQuestionCircle as farQuestionCircle,
+// } from '@fortawesome/free-regular-svg-icons';
+
+/* eslint-enable import/first, import/order */
+faLibrary.add(
+  faCheck,
+  faExclamationTriangle,
+  faTrash,
+);
+//
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 new Vue({
   router,
