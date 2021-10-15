1908178daed819a9377c686511f4be44eef0d1bb
Daniel Williams <danny.h.williams@hotmail.com>
2021-10-15 23:36:42 +0100

    feat: when logged in redirect back to shop

--- frontend/src/screens/Login.tsx
+++ frontend/src/screens/Login.tsx
@@ -1,11 +1,11 @@
-import React, { ChangeEvent, FormEvent, useState } from "react";
-import { useHistory } from "react-router";
-import { login, logout, useAuth } from "../state/UserContext";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Redirect, useHistory } from "react-router";
+import { login, useAuth } from "../state/UserContext";
 
 export const Login = () => {
   const {
     dispatch,
-    state: { auth, user },
+    state: { auth },
   } = useAuth();
   const [userName, setUserName] = useState("");
 
@@ -23,20 +23,7 @@ export const Login = () => {
     <div>
       <h1>Login</h1>
       {auth ? (
-        <>
-          <p>{user?.user_id} you're already logged in</p>
-          <button
-            type="button"
-            onClick={() => {
-              logout(dispatch, () => history.push("/login"));
-            }}
-          >
-            Logout
-          </button>
-          <button type="button" onClick={() => history.push("/")}>
-            back to shop
-          </button>
-        </>
+        <Redirect to="/" />
       ) : (
         <form onSubmit={onSubmit}>
           <label>
